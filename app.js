36d4c08e2cd6c473f6217a16e13a83b6f66bdf47
CamB17 <camcodes@Camerons-MacBook-Pro.local>
2017-04-18 00:10:23 -0600

    Completes all tasks

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var url = "mongodb://localhost:27017/restaurant_db";
 
 mongo.connect(url, function(err, db){
   var collection = db.collection('restaurants');
-  
+   // var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
+
   //Task1 Search
   //For loop to search for the exact rest you want
 //   var searchRestaurant = prompt("Search Restaurants:");
@@ -14,38 +15,42 @@ mongo.connect(url, function(err, db){
   
   //Task 2 Create/Add
   //Give User prompts to create and add their own restaurant
-//     var addRestName = prompt("Add Restaurant name: ");
-//     var addRestStreet = prompt("Enter new address: ");
-//     var addRestZipcode = prompt("Enter new zip: ");
-//     var addRestYelp = prompt("Add Yelp Url: ");
+    var addRestName = prompt("Add Restaurant name: ");
+    var addRestStreet = prompt("Enter new address: ");
+    var addRestZipcode = prompt("Enter new zip: ");
+    var addRestYelp = prompt("Add Yelp Url: ");
 	   
-//      collection.insert({
-//        name: addRestName,
-//        street: addRestStreet,
-//        zipcode: addRestZipcode,
-//        yelp: addRestYelp
+     collection.insert({
+       name: addRestName,
+       address: {
+       street: addRestStreet,
+       zipcode: addRestZipcode,
+       },
+       yelp: addRestYelp
     
-// });
+});
  
 
 	
 
 	//Task 3 Edit
-	//   var editRestName = prompt("Update Restaurant name: ");
- //    var editRestStreet = prompt("Update address: ");
- //    var editRestZipcode = prompt("Update zip: ");
- //    var editRestYelp = prompt("Update Yelp Url: ");
- //      //create new edit collection
- //      collection.update(
- //        { name: editRestaurant },
- //      {
- //        name: editRestName,
- //        street: editRestStreet,
- //        zipcode: editRestZipcode,
- //        yelp: editRestYelp
- //      }
- //  );
- //      console.log(editRestaurant);
+	  var editRestName = prompt("Update Restaurant name: ");
+    var editRestStreet = prompt("Update address: ");
+    var editRestZipcode = prompt("Update zip: ");
+    var editRestYelp = prompt("Update Yelp Url: ");
+      //create new edit collection
+      collection.update(
+        { name: editRestaurant },
+      {
+        name: editRestName,
+        address: {
+        street: editRestStreet,
+        zipcode: editRestZipcode,
+        },
+        yelp: editRestYelp
+      }
+  );
+      console.log(editRestaurant);
 
 
   //Task 4 Delete
